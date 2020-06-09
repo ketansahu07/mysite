2919ef745bf8fd81182ad19025fe715ebd495398
Ketan Sahu <ketankumar120@gmail.com>
2020-06-09 18:17:15 +0530

    models added

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Tutorial
 # Register your models here.
+
+class TutorialAdmin(admin.ModelAdmin):
+	fieldsets = [
+		("Title/date", {"fields": ["tutorial_title", "tutorial_published"]}),
+		("Content", {"fields": ["tutorial_content"]})
+	]
+
+admin.site.register(Tutorial, TutorialAdmin)
