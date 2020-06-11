3ac054958bdcca570646d0d4379bcef17bbb59ae
Ketan Sahu <ketankumar120@gmail.com>
2020-06-12 02:03:07 +0530

    working with foreign key - p.10

--- main/admin.py
+++ main/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-from .models import Tutorial
+from .models import Tutorial, TutorialCategory, TutorialSeries
 # Register your models here.
 
 class TutorialAdmin(admin.ModelAdmin):
 	fieldsets = [
 		("Title/date", {"fields": ["tutorial_title", "tutorial_published"]}),
+		("URL", {"fields": ["tutorial_slug"]}),
+		("Series", {"fields": ["tutorial_series"]}),
 		("Content", {"fields": ["tutorial_content"]})
 	]
+
+admin.site.register(TutorialCategory)
+admin.site.register(TutorialSeries)
 
 admin.site.register(Tutorial, TutorialAdmin)
